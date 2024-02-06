b235088ff582f8370731e4184f86661fec75697a
Ulises Vargas <uli.vargas02@gmail.com>
2024-02-06 00:17:18 -0600

    chore: Agrego el role a el mock de usuarios desde el enum

--- tests/fixtures/mock/user.ts
+++ tests/fixtures/mock/user.ts
@@ -1,9 +1,10 @@
-import { UserEntity } from '../../../src/domain/entities/user.entity'
+import { Role, UserEntity } from '../../../src/domain/entities/user.entity'
 
 export const user: UserEntity = {
   id: '71d27aae-e958-4a6f-9b1a-f68e9d2d1d64',
   name: 'Jhon',
   lastname: 'Doe',
   email: 'jhon@email.com',
-  password: 'pass123'
+  password: 'pass123',
+  role: Role.user
 }
